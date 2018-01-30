beebf5b71bc91b8163987e3ac9baf89c99c7bc21
Kris <ksmolucha9928@gmail.com>
2018-01-30 20:59:40 +0100

    Edit things

--- page/add_o.php
+++ page/add_o.php
@@ -1,4 +1,4 @@
-<div class="col-lg-9">
+<div class="col-lg-9"></p>
 <h4 class="muted">Wyraź swoją opinie na temat wizyty. Będzie nam niezmiernie miło :)</h4></p>
   			<div class="form-group row has-success">
 			  <label for="inputHorizontalSuccess" class="col-sm-3 col-form-label">Opinia</label>
@@ -6,13 +6,13 @@
 			    <textarea class="form-control" id="exampleTextarea" rows="4" placeholder="Wprowadź swoją opinię"></textarea>
 				</div>
 			</div>
-				<div class="form-group row has-danger">	
+				<div class="form-group row has-danger">
 				<div class="col-sm-3"></div>
 				<div class="col-sm-8">
 						<button type="submit" class="btn btn-outline-danger  btn-block">Dodaj opinię</button>
 				</div>
 				</div>
-			  
-			
+
+
 
 </div>
