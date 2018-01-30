6a493e70105e3714598453777b687b8e54e2fdbe
Kris <ksmolucha9928@gmail.com>
2018-01-30 21:21:12 +0100

    Code look fix

--- index.php
+++ index.php
@@ -2,20 +2,19 @@
     include("inc/header.php");
     include("inc/nav.php");
 
-
     $page = isset($_GET['page']) ? trim(strtolower($_GET['page']))       : "home";
 
     $allowedPages = array(
-        'home'     	=> 'page/home.php',
-        'error'     	=> 'page/err.php',
-        'zarejestruj'    	=> 'page/reg.php',
+        'home'            => 'page/home.php',
+        'error'           => 'page/err.php',
+        'zarejestruj'     => 'page/reg.php',
         'krwiodawstwo'    => 'page/krw.php',
-        'zaloguj'     	=> 'page/login.php',
-        'opinie'    	=> 'page/opinion.php',
-        'nagrody'    => 'page/reward.php',
-        'dodaj_opinie'     	=> 'page/add_o.php',
-        'zmien_dane'    	=> 'page/change_d.php',
-        'odbierz_nagrode'    => 'page/take_r.php',
+        'zaloguj'         => 'page/login.php',
+        'opinie'          => 'page/opinion.php',
+        'nagrody'         => 'page/reward.php',
+        'dodaj_opinie'    => 'page/add_o.php',
+        'zmien_dane'      => 'page/change_d.php',
+        'odbierz_nagrode' => 'page/take_r.php',
     );
 
     include(isset($allowedPages[$page]) ? $allowedPages[$page] : $allowedPages["error"] );
--- page/opinion.php
+++ page/opinion.php
@@ -1,42 +1,33 @@
 <div class="col-lg-9">
+    <?php
+    require_once "cnt/connect.php";
 
-
-
-<?php
-require_once "cnt/connect.php";
-
-// Create connection
-$conn = new mysqli($host1, $db_user, $db_password, $db_name);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "SELECT imie, nazwisko, opinia, data FROM opinion JOIN uzytkownicy on opinion.u_id = uzytkownicy.u_id";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo '<div class="card">
-  <div class="card-body">
-    <h5 class="card-title">'; 
-		echo $row["imie"];
-		echo '</h5><h6 class="card-subtitle mb-2 text-muted">';
-		echo $row["data"];
-		echo '</h6><hr><p class="card-text">';
-		echo $row["opinia"];
-		echo '</p></div></div></p>';
+    // Create connection
+    $conn = new mysqli($host1, $db_user, $db_password, $db_name);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
-} else {
-    echo "0 results";
-}
-$conn->close();
-?>
 
+    $sql = "SELECT imie, nazwisko, opinia, data FROM opinion JOIN uzytkownicy on opinion.u_id = uzytkownicy.u_id";
+    $result = $conn->query($sql);
 
-
-
-
-   
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo '<div class="card">
+                  <div class="card-body">
+                  <h5 class="card-title">';
+            echo $row["imie"];
+            echo '</h5><h6 class="card-subtitle mb-2 text-muted">';
+            echo $row["data"];
+            echo '</h6><hr><p class="card-text">';
+            echo $row["opinia"];
+            echo '</p></div></div></p>';
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+    ?>
 </div>
